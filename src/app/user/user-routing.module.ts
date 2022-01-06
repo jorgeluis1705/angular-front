4d37f46cb8907be8f93442f4a72daa5178bbb5a7
jorge Luis Pena Real <falmemor@gmail.com>
2022-01-05 23:59:57 -0600

    resolve added success

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,3 +1,4 @@
+import { UsersResolver } from './shared/resolvers/users.resolver';
 import { UserComponent } from './components/user/user.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -5,6 +6,7 @@ import { UsersComponent } from './components/users/users.component';
 
 const routes: Routes = [
   {
+    resolve: [UsersResolver],
     path: '',
     component: UsersComponent,
     children: [
