486e0c4021318c36883a7e660e6f5a9f8e44e59b
jorge Luis Pena Real <falmemor@gmail.com>
2022-01-06 11:40:09 -0600

    redirect works

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,3 +1,4 @@
+import { UsersMainComponent } from './components/users-main/users-main.component';
 import { UsersResolver } from './shared/resolvers/users.resolver';
 import { UserComponent } from './components/user/user.component';
 import { NgModule } from '@angular/core';
@@ -5,11 +6,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './components/users/users.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
   {
-    resolve: { users: UsersResolver },
     path: '',
-    component: UsersComponent,
+    component: UsersMainComponent,
     children: [
+      {
+        path: 'list',
+        component: UsersComponent,
+        resolve: { users: UsersResolver },
+      },
       {
         path: 'detalles',
         component: UserComponent,
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
+import { UsersMainComponent } from './components/users-main/users-main.component';
 
 
 @NgModule({
   declarations: [
     UserComponent,
-    UsersComponent
+    UsersComponent,
+    UsersMainComponent
   ],
   imports: [
     CommonModule,
