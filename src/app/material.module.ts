fda465d8fd4fd7cb90812899ba43f4ff24f52c6d
jorge Luis Pena Real <falmemor@gmail.com>
2022-01-06 11:52:35 -0600

    TODO table

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,10 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatSliderModule, MatButtonModule],
-  exports: [MatSliderModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatTableModule,
+  ],
+  exports: [
+    MatSliderModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatTableModule,
+  ],
 })
 export class MaterialModule {}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,3 +1,4 @@
+import { MaterialModule } from './../material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -6,16 +7,8 @@ import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
 import { UsersMainComponent } from './components/users-main/users-main.component';
 
-
 @NgModule({
-  declarations: [
-    UserComponent,
-    UsersComponent,
-    UsersMainComponent
-  ],
-  imports: [
-    CommonModule,
-    UserRoutingModule
-  ]
+  declarations: [UserComponent, UsersComponent, UsersMainComponent],
+  imports: [CommonModule, MaterialModule, UserRoutingModule],
 })
-export class UserModule { }
+export class UserModule {}
